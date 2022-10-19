85729397edcbefe528fd9d03f285cc42874817c7
Gagan Malvi <malvigagan@gmail.com>
2022-10-19 16:52:57 +0300

    Fix optional library declaration.
    * As of Android 12, library checks are enforced, and this declaration is necessary.

--- Android.mk
+++ Android.mk
@@ -6,6 +6,7 @@ LOCAL_MODULE := PhotonCamera
 LOCAL_MODULE_CLASS := APPS
 LOCAL_PRODUCT_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_OPTIONAL_USES_LIBRARIES := androidx.window.extensions androidx.window.sidecar
 LOCAL_SRC_FILES := PhotonCamera.apk
 
 include $(BUILD_PREBUILT)
